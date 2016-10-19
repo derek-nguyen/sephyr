506bcac07af2874912cc2f4ff7475ed7ef0f8b79
Arjun Nayak <arjunglobal09@gmail.com>
2016-10-19 01:46:17 -0700

    main buttons direct to right area. addl: added more consistent styling, moved font declarations to fonts.css, custom styling for landing in stylesheet.css

--- app/views.py
+++ app/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-import subprocess
 
 from .models import LandingSubscriber
 from .models import LandingSubscriberForm
 
-def index(request):
+import re
+
+def index( request ):
 	return render(request, 'index.html', None)
 
-def thankyou(request):
+def thankyou( request ):
     # if this is a POST request, we need to process the form data
     if request.method == 'POST':
         form = LandingSubscriberForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data['email']
+            if email and len(email):
+                check = re.match()
             first_name = form.cleaned_data['first_name']
             last_name = form.cleaned_data['last_name']
             sub = LandingSubscriber(email=email)
@@ -21,11 +24,19 @@ def thankyou(request):
                 sub.first_name = first_name
                 sub.last_name = last_name
             sub.save()
-            return render(request, 'thankyou.html', {'sub':sub})
+        return render(request, 'thankyou.html', {'sub':sub})
     else:
         return redirect('index')
 
-def subscribe(request):
+def subscribe( request ):
     return HttpResponse("got to subscribe function")
 	
-	
+
+def validateEmail( email ):
+    from django.core.validators import validate_email
+    from django.core.exceptions import ValidationError
+    try:
+        validate_email(email)
+        return True
+    except ValidationError:
+        return False

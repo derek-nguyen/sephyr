2f0744e916c49718b3d7f6b3d41552e759cb924c
Arjun Nayak <arjunglobal09@gmail.com>
2016-11-27 04:08:38 -0800

    adding provider user, changing landing subscriber table name

--- app/models.py
+++ app/models.py
@@ -1,10 +1,18 @@
 from django.db import models
 from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+class Provider(models.Model):
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	address = models.CharField(max_length=100)
 
 class LandingSubscriber(models.Model):
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
     email = models.EmailField(max_length=254, blank=True)
+
+    class Meta:
+      db_table = "landing_subscriber"
 
 class LandingSubscriberForm(ModelForm):
     class Meta:
